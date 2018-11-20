2382dfff74b8ecfc932427d2f15bb1ddf1ebceff
Scott Whittaker <scott.whittaker@massiveinteractive.com>
2018-11-20 08:06:55 +0000

    chore: inline css

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,16 @@
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const CleanCSS = require("clean-css");
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('img');
   eleventyConfig.addPassthroughCopy('css');
   eleventyConfig.addPlugin(syntaxHighlight);
 
-eleventyConfig.addFilter('format_date', obj => {
+  eleventyConfig.addFilter('format_date', obj => {
     return new Date(obj).toDateString();
+  });
+
+  eleventyConfig.addFilter("cssmin", function(code) {
+    return new CleanCSS({}).minify(code).styles;
   });
 };
