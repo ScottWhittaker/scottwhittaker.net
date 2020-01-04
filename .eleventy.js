c01a6f326c133efc816e959156bedfc54110a670
Scott Whittaker <scott.whittaker@massiveinteractive.com>
2020-01-04 12:14:28 +0000

    fix: use time element for dates providing more semantic markup

--- .eleventy.js
+++ .eleventy.js
@@ -7,8 +7,18 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy({'static': '/'});
   eleventyConfig.addPlugin(syntaxHighlight);
 
-  eleventyConfig.addFilter('format_date', obj => {
+  eleventyConfig.addFilter('format_date_to_date_string', obj => {
     return new Date(obj).toDateString();
+  });
+
+  eleventyConfig.addFilter('format_date_to_iso_string', obj => {
+    // TODO
+    // when logged to console `obj` is `2019-12-11T00:00:00.000Z`.
+    // if we render the date in the browser using {{post.data.date}}
+    // we get `Wed Dec 11 2019 00:00:00 GMT+0000 (GMT)`.
+    // not sure why this occurs hence using this filter to ensure we get iso date to use as the
+    // `datetime` attribute of the `time` element.
+    return new Date(obj).toISOString();
   });
 
   eleventyConfig.addFilter("cssmin", function(code) {
